ae4da86da660ead328a10d1deadec7c5385a1989
Quitesimple12 <122361337+Quitesimple12@users.noreply.github.com>
2024-03-04 18:33:47 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+const RedLight = ({selected, onClick}) => {
+	return <div className={`bg-danger rounded-circle ${selected ? 'selected' : ''}`}
+	style={{width: "125px", height: "125px", position: "absolute", top: "10px", left: "50%", transform: "translateX(-50%)"}} onClick={onClick}></div>
+}
+
+const YellowLight = ({selected, onClick}) => {
+	return <div className={`bg-warning rounded-circle ${selected ? 'selected' : ''}`}
+	style={{width: "125px", height: "125px", position: "absolute", top: "140px", left: "50%", transform: "translateX(-50%)"}} onClick={onClick}></div>
+}
+
+const GreenLight = ({selected, onClick}) => {
+	return <div className={`bg-success rounded-circle ${selected ? 'selected' : ''}`}
+	style={{width: "125px", height: "125px", position: "absolute", top: "270px", left: "50%", transform: "translateX(-50%)"}} onClick={onClick}></div>
+}
 //create your first component
 const Home = () => {
+	const [redlightselected, setredlightselected] = useState(false);
+	const [yellowlightselected, setyellowlightselected] = useState(false)
+	const [greenlightselected, setgreenlightselected] = useState(false)
+	const toggleRedLight = () => {
+		setredlightselected(true)
+		setyellowlightselected(false)
+		setgreenlightselected(false)
+	}
+	const toggleYellowLight = () => {
+		setyellowlightselected(true)
+		setgreenlightselected(false)
+		setredlightselected(false)
+	}
+	const toggleGreenLight = () => {
+		setgreenlightselected(true)
+		setredlightselected(false)
+		setyellowlightselected(false)
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div style = {{display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center",}}>
+		<div>
+			<div className="card bg-dark" style={{height: "200px", width: "10px"}}>
+  				<div className="card-body">
+  				</div>
+			</div>
+		</div>
+		<div>
+			<div className="card bg-dark" style={{height: "400px", width: "200px"}}>
+				<div className="card-body" style={{ position: "relative" }}>
+					<RedLight selected = {redlightselected} onClick={toggleRedLight} />
+					<YellowLight selected = {yellowlightselected} onClick={toggleYellowLight}/>
+					<GreenLight selected = {greenlightselected} onClick={toggleGreenLight}/>
+				</div>
+			</div>
+		</div>
 		</div>
 	);
 };
